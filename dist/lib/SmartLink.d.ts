c9beba466169898dfbed7130246a8ad2a8a94af1
lion <chengbin@3578.cn>
2024-12-09 12:55:31 +0800

    u

--- dist/lib/SmartLink.d.ts
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from 'react';
-interface SmartLinkProps {
-    href: string;
-    children: ReactNode;
-}
-export default function SmartLink({ href, children }: SmartLinkProps): import("react/jsx-runtime").JSX.Element;
-export {};
